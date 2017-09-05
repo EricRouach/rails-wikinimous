579462e81b069fb9416d0d3a4cd42c95d2c38cd8
Eric Rouach <erouach@hotmail.fr>
2017-09-05 16:38:19 +0200

    wikinimous

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  resources :articles
+
+  # get '/articles/index', to: 'article#index'
+
+  # get '/articles/show/:id', to: 'article#show'
+
+  # get '/articles/new', to: 'article#new'
+
+  # post '/articles', to: 'article#create'
+
+  # get '/articles/:id/edit', to: 'article#edit'
+
+  # patch '/articles/:id', to: 'article#update'
+
+  # delete '/articles/:id', to: 'article#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
